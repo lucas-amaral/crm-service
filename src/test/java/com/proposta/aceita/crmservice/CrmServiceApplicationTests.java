b95d318feeb636156db6866699a777caa3dfcf9e
luke <amaral@devgrid.co.uk>
2020-03-20 00:29:20 -0300

    git init

---
+++ src/test/java/com/proposta/aceita/crmservice/CrmServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proposta.aceita.crmservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
