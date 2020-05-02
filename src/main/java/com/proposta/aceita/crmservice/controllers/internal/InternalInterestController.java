1dc4f1cb16dfbbaad28cffb5869534e3dacf1ba1
luke <amaral@devgrid.co.uk>
2020-05-02 20:32:05 -0300

    Fix internal endpoints path

--- src/main/java/com/proposta/aceita/crmservice/controllers/internal/InternalInterestController.java
+++ src/main/java/com/proposta/aceita/crmservice/controllers/internal/InternalInterestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("internal/interests/users")
+@RequestMapping("internal/interests")
 public class InternalInterestController {
 
     private final InterestService interestService;
--- src/main/java/com/proposta/aceita/crmservice/controllers/internal/InternalPropertyController.java
+++ src/main/java/com/proposta/aceita/crmservice/controllers/internal/InternalPropertyController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("internal/properties/users")
+@RequestMapping("internal/properties")
 public class InternalPropertyController {
 
     private final PropertyService propertyService;
